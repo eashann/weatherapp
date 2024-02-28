446912158fab7509f5936c0b8cf278c94518cfd4
Eashan <eashan.ashrafulzaman@live.com>
2024-02-28 22:55:44 +0600

    Initial Commit

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by Eashan on 28/2/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
